aeadc217f8a7daa9c6c460e72fa5dfb39a1a45ef
владимир назаров <volottnazarov@mail.ru>
2023-09-07 22:46:55 +0300

    программа вывода четных чисел от 1 до N

---
+++ Program_All_Even_Numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая на вход принимает число N, а на выходе показывает
+// все четные числа от 1 до N.
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int number = 1;
+while(number<N)
+{
+    if(number%2 == 0)
+    {
+        Console.WriteLine(number);
+    }
+number = number + 1 ;
+}
