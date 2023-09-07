e152df23a5e6cd77d5bd87e3c62d71c822928d71
владимир назаров <volottnazarov@mail.ru>
2023-09-07 12:20:12 +0300

    программа для выявления чётного числа

---
+++ Program_Even_Number/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа, которая на вход принимает число и выдаёт, является ли число чётным 
+//(делится ли оно на два без остатка).
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number%2==0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число не является четным");
+}
