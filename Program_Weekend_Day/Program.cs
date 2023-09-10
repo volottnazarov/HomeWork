a96f64a23158af76fc7dc15fc2a2bc4a23656317
владимир назаров <volottnazarov@mail.ru>
2023-09-11 01:00:33 +0300

    выходные дни в неделе

---
+++ Program_Weekend_Day/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Программа, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+int AWeek(string message)
+{
+    Console.Write(message);
+    string day = Console.ReadLine()!;
+    int number_day = Convert.ToInt32(day);
+    return number_day;
+}
+int WeekEnd(int number)
+{
+    if(number<6)
+    {
+        Console.WriteLine("Будний день");
+    }
+    else
+    {
+        Console.WriteLine("Выходной день");
+    }
+    return number;
+}
+int number = AWeek("Введите номер дня недели от 1 до 7: ");
+WeekEnd(number);
