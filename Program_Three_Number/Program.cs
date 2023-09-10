ac0f43bc3405ac86699f2c0be1a63a96b1f586ee
владимир назаров <volottnazarov@mail.ru>
2023-09-11 00:02:35 +0300

    вычисление третьей цыфры в числе

---
+++ Program_Three_Number/Program.cs
@@ -0,0 +1,26 @@
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число:");
+bool MinValue(int number)
+{
+    if (number<100)
+    {
+        Console.WriteLine("В введенном числе нет третьей цыфры");
+         Console.WriteLine("Введите число больше 99:");
+         return false;
+    }
+    return true;
+}
+int number = Convert.ToInt32(Console.ReadLine());
+int ThreeNumberPosition(int number)
+{
+    while(number>999)
+    {
+        number = number/10;
+    }
+return number%10;
+}
+if(MinValue(number))
+{
+    Console.WriteLine("Третья цифра введенного числа:" + ThreeNumberPosition(number));
+}
+
