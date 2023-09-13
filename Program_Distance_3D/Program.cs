67984ae0a08ce405c512b6d88a2ffeb8f5db5cd7
владимир назаров <volottnazarov@mail.ru>
2023-09-13 12:06:05 +0300

    нахождение расстояния между двух точек в 3D

---
+++ Program_Distance_3D/Program.cs
@@ -0,0 +1,54 @@
+﻿// Внутри класса Answer напишите метод DistanceBetweenPoints,
+// который принимает на вход координаты двух точек pointA и pointB в виде массива целых чисел,
+// и возвращает расстояние между ними в 3D пространстве.
+using System;
+
+public class Answer
+{
+  static int Square(int number)
+  {
+    return number*number;
+  }
+  static int SumSquare(int[]pointA, int[]pointB)
+  {
+    int sum = 0;
+    for(int i = 0; i < pointA.Length; i++)
+    {
+      sum += Square(pointA[i] - pointB[i]);
+    }
+    return sum;
+  }
+    private static double Length(int[] pointA, int[] pointB)
+    {
+      // Введите свое решение ниже
+        double result = Math.Sqrt(SumSquare(pointA, pointB));
+        return result;
+
+    }
+
+  // Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args) {
+        int x1, x2, x3, y1, y2, y3;
+
+        if (args.Length >= 6) {
+            x1 = int.Parse(args[0]);
+            x2 = int.Parse(args[1]);
+            x3 = int.Parse(args[2]);
+            y1 = int.Parse(args[3]);
+            y2 = int.Parse(args[4]);
+            y3 = int.Parse(args[5]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            x1 = 3;
+            x2 = 6;
+            x3 = 8;
+            y1 = 2;
+            y2 = 1;
+            y3 = -7;
+        }
+
+        // Не удаляйте строки ниже
+        double result = Length(new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
+        Console.WriteLine($"{result:F2}");
+    }
+}
