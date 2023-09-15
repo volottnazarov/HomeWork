9019f0423b736deb7efcf2486887908fcd5d8be3
владимир назаров <volottnazarov@mail.ru>
2023-09-15 09:31:06 +0300

    добавили программу, сумма от 1 до А

---
+++ Function/Program.cs
@@ -0,0 +1,30 @@
+﻿// Изучение и практика по функциям
+//=====Приветствие и умножение двух чисел
+// static void WriteHello(string str)
+// {
+//     Console.WriteLine(str);
+// }
+// WriteHello("Привет");
+// static int Multiply(int num_1,int num_2)
+// {
+//     return num_1*num_2;
+// }
+// int num_1 = Convert.ToInt32(Console.ReadLine());
+// int num_2 = Convert.ToInt32(Console.ReadLine());
+// int result = Multiply(num_1,num_2);
+// Console.WriteLine("Результат:"+result);
+//=====Программа , которая на вход принимает число А , на выходе сумму от 1 до А
+// int Sum(int A)
+// {
+//     int sum = 0;
+//     for(int i = 0 ; i <= A ; i++)
+//     {
+//         sum = sum + i;
+//     }
+//     return sum;
+// }
+// Console.WriteLine("Введите число :");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Sum(A));
+
+
