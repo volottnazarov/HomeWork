f760161cfba0f5fb4e6efd9d8b0737628171221c
владимир назаров <volottnazarov@mail.ru>
2023-09-15 10:36:12 +0300

    цикл на входе A и B, на выходе A в степени B

---
+++ Program_Natural_Degree/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int NaturalDegree(int A, int B)
+{
+    int degree = 1;
+    for(int i = 1; i <= B; i++)
+    {
+        degree = degree*A;
+    }
+    return degree;
+}
+Console.WriteLine("Введите два числа: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int result = NaturalDegree(A , B);
+Console.WriteLine(result);
