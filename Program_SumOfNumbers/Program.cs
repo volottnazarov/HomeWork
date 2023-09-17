791eda0c0a2595bb14699c3e9cf2df7562c94953
владимир назаров <volottnazarov@mail.ru>
2023-09-17 17:51:19 +0300

    программа суммы всех цифр в числе

---
+++ Program_SumOfNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+int Promt(string message)
+{
+    Console.Write(message);
+    string readInput = Console.ReadLine()!;
+    int result = int.Parse(readInput);
+    return result;
+}
+int SumDigit(int number)
+{
+    int sum = 0;
+    while(number>0)
+    {
+        sum = sum + number%10;
+        number = number/10;
+    }
+    return sum;
+}
+int number = Promt("Введите число: ");
+Console.WriteLine(SumDigit(number));
