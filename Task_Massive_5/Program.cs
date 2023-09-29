ad63a76dc61eb3412d1c03a98dd3416b6bb98223
владимир назаров <volottnazarov@mail.ru>
2023-09-29 23:53:00 +0300

    Нахождение строки в массиве с наименьшей суммой эл-ов

---
+++ Task_Massive_5/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, 
+//которая будет находить строку с наименьшей суммой элементов.
+
+int[,]GetArray(int rows, int cols, int minValue, int maxValue)
+{
+    int[,]array = new int[rows, cols];
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,]array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+         Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SumInLines(int[,] array)
+{
+    int[] sumInLines = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {  
+            sumInLines[i] += array[i, j];
+        }
+    }
+    int minPosition= 0;
+    for (int i = 0; i < sumInLines.Length; i++)
+    {
+        if (sumInLines[minPosition] > sumInLines[i])
+         minPosition = i;
+    }
+    Console.WriteLine($"Строка с наименьшей суммой элементов: {minPosition + 1}");  
+}
+
+int[,]array = GetArray(4,3,1,10);
+PrintArray(array);
+SumInLines(array);
