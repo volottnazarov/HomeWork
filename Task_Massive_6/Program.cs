9adaa827187f34299cea4ae587ed26c38cff1d90
владимир назаров <volottnazarov@mail.ru>
2023-09-30 00:40:09 +0300

    упорядочиваем по убыванию элементы каждой строки двумерного массива

---
+++ Task_Massive_6/Program.cs
@@ -0,0 +1,48 @@
+﻿// Программа, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+int[,]GetArray(int rows, int cols, int minValue, int maxValue)
+{
+    int[,]array = new int[rows, cols];
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,]array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+         Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+void SortSelection(int[,]array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1)-1; k++)
+            {
+                 if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }    
+        
+            }
+        }
+    }
+}            
+int[,]array = GetArray(4,4,1,10);
+PrintArray(array);
+SortSelection(array);
+PrintArray(array);
