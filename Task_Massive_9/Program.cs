d5fab4568a4c1454f28893c5af8b53fd7b0b2909
владимир назаров <volottnazarov@mail.ru>
2023-09-30 22:59:53 +0300

    Вывод массива с добавлением индекса каждого эл-та

---
+++ Task_Massive_9/Program.cs
@@ -0,0 +1,37 @@
+﻿// Программа, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+//Массив размером 2 x 2 x 2
+
+static int[,,]Get3DMatrix(int x, int y, int z, int min, int max)
+       {
+        int[,,] matrix = new int[x,y,z];
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                for (int k = 0; k < matrix.GetLength(2); k++)
+                {  
+                     matrix[i,j,k] = new Random().Next(min, max +1); 
+                }
+            }
+        }
+        return matrix;    
+       }
+static void Print3DMatrix(int[,,]array)
+    {
+     for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                for (int k = 0; k < array.GetLength(2); k++)
+                {
+                    
+                    Console.Write(array[i, j, k]+"("+ i +","+ j +"," + k + ")") ;
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }    
+    int[,,]matrix = Get3DMatrix(2,2,2,1,100);
+    Print3DMatrix(matrix);
+
